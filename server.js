9f470155500979ba1df424bce3864df3b90903e1
mnofrizal <mnofrizal1996@gmail.com>
2025-06-17 12:15:42 +0700

    ok

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1286;
 
 // Middleware
 app.use(cors());
